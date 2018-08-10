19a68dc60c4f9e93b552dec8cd1edda50b386812
Graham Smith <grahamas@gmail.com>
2018-08-10 12:42:30 -0500

    Add analysis file

---
+++ analysis.R
@@ -0,0 +1,11 @@
+#
+# Author: Graham Smith
+#
+# Analyses statistics of bent coin lottery
+#
+
+# Load lottery function(s)
+# If this fails with "No such file or directory",
+# then "Session -> Set Working Directory -> To Source File Location"
+source("lottery.R")
+
