6c92657ffa7afac44f1c8f375b0d0532f12e1e0d
Urszula Cichon <ucichon@uchicago.edu>
2019-09-11 15:04:31 -0400

    made some variables more intuitive
    arr, magic numbers

--- lottery.R
+++ lottery.R
@@ -11,15 +11,19 @@ library(stats)
 
 # Small edit! from class 1!
 
-arr <- array(0,10)
-for (i in 1:10){
-  arr[i] <- if (runif(1) < 0.1) 1 else 0
+num_of_coin_flips <- 10
+num_of_tickets <- 100
+prob_head <- 0.1
+
+Lottery_ticket <- array(0,num_of_coin_flips)
+for (i in 1:num_of_coin_flips){
+  Lottery_ticket[i] <- if (runif(1) < prob_head) 1 else 0
 }
 
-for (j in 1:100){
-  arr <- array(0,10)
-  for (i in 1:10){
-    arr[i] <- if (runif(1) < 0.1) 1 else 0
+for (j in 1:num_of_tickets){
+  Lottery_ticket <- array(0,num_of_coin_flips)
+  for (i in 1:num_of_coin_flips){
+    Lottery_ticket[i] <- if (runif(1) < 0.1) 1 else 0
   }
-  print(arr)
+  print(Lottery_ticket)
 }
